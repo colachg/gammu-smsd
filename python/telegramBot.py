4b3832cf0008b08b54722c9f568e4f8723b2840e
colachg <colachg@gmail.com>
2021-06-19 16:22:14 +0800

    Update bot script.

--- python/telegramBot.py
+++ python/telegramBot.py
@@ -12,8 +12,8 @@ from aiogram.utils.executor import start_webhook
 
 # webhook settings
 WEBHOOK_HOST = os.environ['WEBHOOK_HOST']
-WEBHOOK_PATH = os.environ['CHANIFY_TOKEN']
-WEBHOOK_URL = f"{WEBHOOK_HOST}/{WEBHOOK_PATH}"
+WEBHOOK_PATH =  '/' + os.environ['TELEGRAM_BOT_TOKEN']
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'  # or ip
